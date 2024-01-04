5a7fa109d2ff831068720974b7c87ff924503952
Izanderson Florencio <izandersonf@gmail.com>
2024-01-04 11:38:59 -0300

    fix: update text

--- src/app/certificates/generate/[email]/CertificatePDF.tsx
+++ src/app/certificates/generate/[email]/CertificatePDF.tsx
@@ -10,6 +10,12 @@ import {
 
 import { ParticipantType } from '@/app/types/certificate.types'
 
+const EVENT_NAME = 'Google I/O Extended 2023 - Natal'
+const EVENT_TYPE = 'presencial'
+const EVENT_DURATION = '10h'
+const EVENT_DATE = '15 de julho de 2023'
+const EVENT_LOCATION = 'SEBRAE RN'
+
 const Certificate = ({ participant }: { participant?: ParticipantType }) => {
   if (!participant) return <p>Participante inválido.</p>
 
@@ -19,6 +25,7 @@ const Certificate = ({ participant }: { participant?: ParticipantType }) => {
         <View style={styles.content}>
           <Image style={styles.image} src="/certificate.jpg" />
           <Text style={styles.name}>{participant?.name}</Text>
+          <Text style={styles.description}>{participant?.type === 'speaker' ? 'palestrou no' : 'participou do'} {EVENT_NAME}, evento {EVENT_TYPE} com {EVENT_DURATION} de duração, realizado no dia {EVENT_DATE}, no {EVENT_LOCATION}</Text>
           <Text style={styles.validatorCode}>Código validador: {participant?.id}</Text>
           <Text style={styles.validatorUrl}>Valide em {window.location.origin}/certificates/validate</Text>
         </View>
@@ -46,6 +53,14 @@ const styles = StyleSheet.create({
     top: '155px',
     textAlign: 'center',
     width: '100%'
+  },
+  description: {
+    fontSize: '14px',
+    position: 'absolute',
+    top: '205px',
+    left: '50px',
+    textAlign: 'center',
+    width: '500px'
   },
   image: {
     width: '600px'
